663a3e5445fdecda3ab5deb847a5d249cca2f06f
jayjayjatplane <chung.ianjay@gmail.com>
2025-06-20 04:00:25 +0000

    [automatic commit] 2025-06-20 04:00:24 UTC 129.127.202.26

---
+++ part1/public/javascripts/scripts.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: '#app',
+    data: {
+        title: 'Dog of the Day',
+        buttonText: 'Reload Image',
+        dogOfTheDayImage: ''
+    },
+    methods: {
+        loadDog: function () {
+            var vm = this;
+            fetch('https://dog.ceo/api/breeds/image/random')
+                .then(function (response) { return response.json(); })
+                .then(function (data) { vm.dogOfTheDayImage = data.message; })
+                .catch(function (error) { console.error('Error loading dog image:', error); });
+        }
+    },
+    mounted: function () {
+        this.loadDog();
+    }
+});
