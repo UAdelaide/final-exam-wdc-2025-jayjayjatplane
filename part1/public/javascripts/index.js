6d2af52b0deca8e2a5b31dea702fd323b1f46b66
jayjayjatplane <chung.ianjay@gmail.com>
2025-06-20 06:06:31 +0000

    [automatic commit] 2025-06-20 06:06:30 UTC 129.127.202.26

--- part1/public/javascripts/index.js
+++ part1/public/javascripts/index.js
@@ -1,22 +1,25 @@
+/* global Vue */
+/* eslint-disable no-console */
+
 const { createApp } = Vue;
 
 createApp({
     data() {
         return {
-            title: 'Dog of the Day',
+            title: 'Dog of the Day Image',
             buttonText: 'Reload Image',
             dogOfTheDayImage: ''
         };
     },
     methods: {
-        loadDog() {
+        loadNewDog() {
             fetch('https://dog.ceo/api/breeds/image/random')
-                .then(res => res.json())
-                .then(data => {
+                .then((res) => res.json())
+                .then((data) => {
                     this.dogOfTheDayImage = data.message;
                 })
-                .catch(err => {
-                    console.error('There was an Error loading dog image:', err);
+                .catch((err) => {
+                    console.error('There was a an error loading the dog image:', err);
                 });
         }
     },
