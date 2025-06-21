f70ef856c20d56d9d59a591c149f0f7ee26e6755
jayjayjatplane <chung.ianjay@gmail.com>
2025-06-21 02:03:07 +0000

    [automatic commit] 2025-06-21 02:03:07 UTC

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -25,5 +25,29 @@ router.get('/api/dogs', async (req, res) => {
   }
 });
 
+router.get('/api/walkrequests/open', async (req, res) => {
+  try {
+    const [rows] = await db.query(`
+        SELECT
+        wr.request_id,
+        d.name AS dog_name,
+        wr.requested_time,
+        wr.duration_minutes,
+        wr.location,
+        u.username AS owner_username
+      FROM WalkRequests wr
+      JOIN Dogs d   ON wr.dog_id   = d.dog_id
+      JOIN Users u  ON d.owner_id  = u.user_id
+      WHERE wr.status = 'open'
+    `);
+
+    return res.json(rows);
+  } catch (err) {
+    console.error('/api/walkrequests/open error:', err);
+    return res.status(500).json({ error: 'Server error' });
+  }
+});
+
+
 module.exports = router;
 
