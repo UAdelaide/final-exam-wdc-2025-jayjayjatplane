2afba31bb754e95f80ff3b4558a57c4d02f01141
jayjayjatplane <chung.ianjay@gmail.com>
2025-06-21 02:01:42 +0000

    [automatic commit] 2025-06-21 02:01:41 UTC

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -2,14 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
 
-router.get('api/dogs', async(req, res) => {
+router.get('/api/dogs', async (req, res) => {
   try {
-    const [rows] = await debug.query('
-      SELECT d.name as dogName')
-  })
+    const [rows] = await db.query(`
+      SELECT
+        d.name AS dog_name,
+        d.size AS size,
+        u.username AS owner_username
+      FROM Dogs d
+      JOIN Users u ON d.owner_id = u.user_id
+    `);
+
+    return res.json(rows);
+  } catch (err) {
+    
+    console.error('/api/dogs error:', err);
+    return res.status(500).json({ error: 'Server error' });
+  }
+});
