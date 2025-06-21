99d206d9c256cb12250bc83356a11d19e25a3863
jayjayjatplane <chung.ianjay@gmail.com>
2025-06-21 02:13:13 +0000

    [automatic commit] 2025-06-21 02:13:13 UTC

--- part1/db.js
+++ part1/db.js
@@ -1,6 +1,8 @@
+const mysql = require('mysql2/promise');
+
 const db = mysql.createPool({
-  host:     'localhost',
-  user:     'root',
+  host: 'localhost',
+  user: 'root',
   password: 'password123',
   database: 'DogWalkService'
 });
