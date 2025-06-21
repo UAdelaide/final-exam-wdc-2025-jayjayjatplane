2dbf325ef52d9386026504293c2d87ba09ae734c
jayjayjatplane <chung.ianjay@gmail.com>
2025-06-21 05:51:53 +0000

    [automatic commit] 2025-06-21 05:51:52 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -88,4 +88,20 @@ router.post('/logout', function (req, res) {
   });
 });
 
+router.get('/dogs', async (req, res) => {
+  try {
+    const owner_id = req.session.user.id;
+    const [rows] = await db.query(`
+SELECT dog_id, name
+FROM Dogs
+WHERE owner_id = ?`,
+      [owner_id]
+    );
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'couldnt load dogs' });
+  }
+});
+
 module.exports = router;
