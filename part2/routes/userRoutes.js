894d3b10dd4e00134e0885d43b472b12bf608a70
jayjayjatplane <chung.ianjay@gmail.com>
2025-06-21 03:29:21 +0000

    [automatic commit] 2025-06-21 03:29:20 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -38,15 +38,13 @@ router.get('/me', (req, res) => {
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
-
   try {
-    const [rows] = await db.query(
-      `SELECT user_id, username, role FROM Users
+    const [rows] = await db.query(`
+      SELECT user_id, username, role FROM Users
       WHERE username = ?
       AND password_hash = ?`,
       [username, password]
     );
-
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
